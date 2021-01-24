4a0edb4d8d7ceb319d5ca5ab6db142d39d3b5418
LiuHaolin <25915043@qq.com>
2021-01-24 11:29:28 +0800

    RCD

---
+++ RCD/RCD.py
@@ -0,0 +1,298 @@
+from matplotlib import pyplot as p
+from lof import LOF, k_distance, distance_euclidean
+import math
+import os
+import string
+import sys
+
+# 读入数据
+source = 'normal_data.csv'
+tmp_instances = []
+instances = []
+tmp_labels = []
+labels = []
+
+with open(source, 'rb') as sourcefp:
+    for line in sourcefp:
+        tmp = line.decode().strip().split(',')
+        coordinate = (float(tmp[1]), int(float(tmp[2])), int(float(tmp[3])), int(float(tmp[4])), float(tmp[5]),
+                      float(tmp[6]), int(float(tmp[7])), float(tmp[8]), float(tmp[9]), float(tmp[10]), float(tmp[11]))
+        label = (int(float(tmp[0])), int(float(tmp[12])))
+        tmp_instances.append(coordinate)
+        tmp_labels.append(label)
+
+# 去掉重复的点
+def updateLabel(index):
+    for i in range(0, len(tmp_labels), 1):
+        if tmp_labels[i][0] == index + 1:
+            tmp_labels.remove(tmp_labels[i])
+            break
+
+pos = []
+for i in range(0, len(tmp_instances), 1):
+    if tmp_instances.count(tmp_instances[i]) > 1:
+        for j in range(0, len(tmp_instances), 1):
+            if tmp_instances[i] == tmp_instances[j] and j != tmp_instances.index(tmp_instances[j]) and j not in pos:
+                updateLabel(j)
+
+for i in tmp_instances:
+    if i not in instances:
+        instances.append(i)
+for i in range(0, len(tmp_labels), 1):
+    labels.append((i+1, tmp_labels[i][1]))
+instancesBackup = instances
+
+for instance in instances:
+    print(instance)
+for label in labels:
+    print(label)
+
+x, y = zip(*labels)
+p.scatter(x, y, 20, color="#0000FF")
+p.show()
+# sys.exit(0)
+
+# KList参数设定
+kLength = int(math.log(len(instances), 2))
+# kLength = int(len(instances) / 2)
+kList = []
+tmp = 1
+# 邻接矩阵M
+M = [[] for i in range(len(instances))]
+pre_c1 = [list() for i in range(len(instances))]
+kinf = [list() for i in range(len(instances))]
+# 结果集合Ures
+group = 0
+Ures = [[] for i in range(len(instances))]
+resLabel = [[] for i in range(len(instances))]
+# 局部结果集合Ur
+Ur = []
+# 颜色数组
+colors = ["#FF0000", "#3300FF", "#00FF00",
+          "#CD7F32", "#FFFF00", "#FFF0F5", "#6A5ACD"]
+
+
+# 初始化KList
+for i in range(0, kLength, 1):
+    tmp = tmp*2
+    kList.append(tmp)
+print("Klist :", kList)
+
+
+# 更新邻接矩阵M
+def update():
+    # 计算邻接矩阵M
+    for instance in instances:
+        instances_value_backup = list(instances)
+        instances_value_backup.remove(instance)
+
+        (k_distance_value, neighbours) = k_distance(
+            len(instances_value_backup), instance, instances_value_backup)
+        M[instances.index(instance)] = neighbours
+
+
+# 计算Kinf
+def cal_K_inf():
+    for instance in instances:
+        kmin = float("inf")
+        for k in kList:
+            value = lof.local_outlier_factor(k, instance)
+            if value < kmin:
+                kmin = value
+                k_inf = k
+        kinf[instances.index(instance)] = k_inf
+        # print(instances.index(instance),'kinf',k_inf)
+    print("Kinf:\n", kinf)
+
+
+def pre_cal_c1():
+    for instance in instances:
+        k_inf = kinf[instances.index(instance)]
+        # if lof.local_outlier_factor(k_inf, instance) == 0:
+        #     pre_c1[instances.index(instance)] = 0
+        # else:
+        pre_c1[instances.index(instance)] = lof.local_outlier_factor(
+            k_inf + 1, instance)/lof.local_outlier_factor(k_inf, instance)
+        # print(instances.index(instance),'c1',pre_c1[instances.index(instance)])
+    print("pre_c1:\n", pre_c1)
+
+
+# 计算C1
+def cal_C1(instance):
+    return pre_c1[instances.index(instance)]
+
+
+# 计算C2
+def cal_C2(instance):
+    sum_c1 = 0
+    index = instances.index(instance)
+    k_inf = kinf[instances.index(instance)]
+
+    for i in range(0, k_inf, 1):
+        if (i < len(instances)-1):
+            b = M[index][i]
+            sum_c1 += pre_c1[instances.index(b)]
+    c2 = sum_c1/k_inf
+    return c2
+
+
+# 计算C3
+def cal_C3(instance):
+    k_inf = kinf[instances.index(instance)]
+    k_avg = (2 + k_inf)/2
+
+    (k1, neighbours) = k_distance(k_inf+1, instance, instances)
+    (k2, neighbours) = k_distance(k_avg, instance, instances)
+
+    c3 = k1/k2
+    return c3
+
+
+# 计算C4
+def cal_C4(instance):
+    index = instances.index(instance)
+    k_inf_a = kinf[instances.index(instance)]
+    k_inf_b = 0
+
+    for i in range(0, k_inf_a, 1):
+        if (i < len(instances)-1):
+            b = M[index][i]
+            k_inf_b += kinf[instances.index(b)]
+    # print(k_inf_a,k_inf_b)
+    mid_value = abs(k_inf_a/(k_inf_b/k_inf_a)-1)
+    c4 = math.exp(mid_value)
+    return c4
+
+
+# 计算C
+def cal_C(instance):
+    c1 = cal_C1(instance)
+    c2 = cal_C2(instance)
+    c3 = cal_C3(instance)
+    c4 = cal_C4(instance)
+    c = (c1*c2*c3*c4)**0.25
+    # print(instances.index(instance),'c',c)
+    return c
+
+
+# 扩展集合
+def expand(center, init_instances):
+    instances_backup = list(init_instances)
+    instances_backup.remove(center)
+
+    index = instances.index(center)
+    avg_distance = 0
+
+    for b in instances_backup:
+        avg_distance += distance_euclidean(center, b)
+    avg_distance = avg_distance/len(instances_backup)
+
+    for b in instances_backup:
+        index_b = instances.index(b)
+        kb = M[index].index(b)
+
+        for i in range(0, kb+1, 1):
+            c = M[index_b][i]
+            if c not in init_instances:
+                distance = distance_euclidean(b, c)
+                if distance < avg_distance:
+                    init_instances.append(c)
+    # print(len(init_instances))
+
+
+# 扩展Klist
+def refine(rc):
+    min_value = float("inf")
+
+    for k in kList:
+        value = (k - len(rc))**2
+        if value < min_value:
+            min_value = value
+            k_min = k
+
+    #k_new = int((k_min + len(rc)) / 2)
+    k_new = len(rc)
+    if k_new not in kList:
+        kList.append(k_new)
+    kList.sort()
+    # print("New klist is \n",kList)
+
+
+while True:
+    print("New instances is \n", instances)
+    lof = LOF(instances)
+    M = [[] for i in range(len(instances))]
+    pre_c1 = []
+    kinf = []
+
+    for i in range(len(instances)):
+        pre_c1.append(0)
+        kinf.append(0)
+    for k in kList:
+        if k >= len(instances):
+            kList[0:kList.index(k)]
+            break
+    print("kList is", kList)
+
+    update()
+    cal_K_inf()
+    pre_cal_c1()
+    Ur.clear()
+
+    # 找到LOF异常点
+    for instance in instances:
+        for k in kList:
+            value = lof.local_outlier_factor(k, instance)
+            if value > 1:
+                Ur.append(instance)
+                print("LOF points", instances.index(instance), k, value)
+                break
+    print("Ur is \n", Ur)
+
+    if Ur:
+        max_value = float("-inf")
+        C = []
+
+        for u in Ur:
+            value = cal_C(u)
+            if value > max_value:
+                max_value = value
+                a = u
+                # print(u,max_value)
+        print("center is ", a, kinf[instances.index(a)])
+        C.append(a)
+        index = instances.index(a)
+        k_inf = kinf[instances.index(a)]
+        for i in range(0, k_inf, 1):
+            if (i < len(instances)-1):
+                b = M[index][i]
+                if b in instances:
+                    C.append(b)
+        for c in C:
+            color = colors[0]
+            p.scatter(c[0], c[1], color=color)
+        p.show()
+
+        expand(a, C)
+        refine(C)
+        Ures[group] = list(C)
+        group += 1
+        remain = set(instances) - set(C)
+        instances = list(remain)
+
+    else:
+        break
+    if len(instances) == 0:
+        break
+for i in range(0, group, 1):
+    for instance in Ures[i]:
+        index = instancesBackup.index(instance)
+        resLabel[i].append(labels[index])
+    print("group", i, "is \n", Ures[i])
+    print("group", i, "label is \n", resLabel[i])
+
+for i in range(0, group, 1):
+    for c in resLabel[i]:
+        color = colors[i]
+        p.scatter(c[0], c[1], color=color)
+p.show()
---
+++ RCD/handleData.py
@@ -0,0 +1,158 @@
+import numpy as np
+import pandas as pd
+import csv
+
+avg  = np.zeros(13)
+stad = np.zeros(13)
+min = np.array([5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0,5000.0])
+max = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+
+def handleData():
+    source = 'test_data_large.csv'
+    result = 'selected_data_large.csv'
+    sourcefp = open(source , 'r')
+    resultfp = open(result , 'w' , newline='')
+    reader = csv.reader(sourcefp)
+    writer = csv.writer(resultfp)
+    
+    count=0   
+    for line in reader:
+        count += 1
+
+        tmp = np.zeros(13) 
+        tmp[0] = int(count)
+        tmp[1] = int(line[0])
+        tmp[2] = int(protocol(line[1]))   
+        tmp[3] = int(service(line[2]))   
+        tmp[4] = int(flag(line[3]))     
+        for i in range(5 , 12 , 1):
+            tmp[i] = int(line[i-1])
+        tmp[12] = int(label(line[41]))   
+        tmp=tmp.astype(np.int32)
+
+        writer.writerow(tmp)
+        # print(count,'status:',tmp[1],tmp[2],tmp[3],tmp[12])
+    sourcefp.close()
+    resultfp.close()
+
+def protocol(type):
+    plist = ['tcp','udp','icmp']
+    if type in plist:
+        return plist.index(type)
+
+def service(type):
+    slist = ['aol','auth','bgp','courier','csnet_ns','ctf','daytime','discard','domain','domain_u',
+        'echo','eco_i','ecr_i','efs','exec','finger','ftp','ftp_data','gopher','harvest','hostnames',
+        'http','http_2784','http_443','http_8001','imap4','IRC','iso_tsap','klogin','kshell','ldap',
+        'link','login','mtp','name','netbios_dgm','netbios_ns','netbios_ssn','netstat','nnsp','nntp',
+        'ntp_u','other','pm_dump','pop_2','pop_3','printer','private','red_i','remote_job','rje','shell',
+        'smtp','sql_net','ssh','sunrpc','supdup','systat','telnet','tftp_u','tim_i','time','urh_i','urp_i',
+        'uucp','uucp_path','vmnet','whois','X11','Z39_50']
+    if type in slist:
+        return slist.index(type)
+
+def flag(type):
+    flist = ['OTH','REJ','RSTO','RSTOS0','RSTR','S0','S1','S2','S3','SF','SH']
+    if type in flist:
+        return flist.index(type)
+
+
+def label(type):
+    llist = ['normal.', 'buffer_overflow.', 'loadmodule.', 'perl.', 'neptune.', 'smurf.',
+        'guess_passwd.', 'pod.', 'teardrop.', 'portsweep.', 'ipsweep.', 'land.', 'ftp_write.',
+        'back.', 'imap.', 'satan.', 'phf.', 'nmap.', 'multihop.', 'warezmaster.', 'warezclient.',
+        'spy.', 'rootkit.']
+    if type in llist:
+        return llist.index(type)
+    else:
+        llist.append(type)
+        return llist.index(type)
+
+def standardization():
+    source = 'selected_data_large.csv'
+    result = 'standard_data_large.csv'
+    sourcefp = open(source , 'r')
+    resultfp = open(result , 'w' , newline='')
+    reader = csv.reader(sourcefp)
+    writer = csv.writer(resultfp)
+
+    contlist = [1,5,6,8,9,10,11]
+    count = 0
+
+    tmp1 = np.zeros(13) 
+    tmp1 = tmp1.astype(np.int64)
+    for line in reader:
+        count += 1
+        for i in contlist:
+            tmp1[i] += int(line[i]) 
+    for i in contlist:
+        avg[i] = round(tmp1[i] / count , 4)
+    
+    sourcefp.seek(0)
+    tmp2 = np.zeros(13) 
+    for line in reader:
+        for i in contlist:
+            tmp2[i] += np.abs(float(line[i]) - avg[i])
+    for i in contlist:
+        stad[i] = round(tmp2[i] / count , 4)
+
+    sourcefp.seek(0)
+    tmp3 = np.zeros(13) 
+    for line in reader:
+        for i in range(0 , 13 , 1):
+            if i in contlist:
+                if float(stad[i]) == 0 or float(avg[i]) == 0:
+                    tmp3[i] = 0
+                else:
+                    tmp3[i] = round((float(line[i])-float(avg[i]))/float(stad[i]) , 4)
+                
+                if tmp3[i] < min[i]:
+                    min[i] = tmp3[i]
+                if tmp3[i] > max[i]:
+                    max[i] = tmp3[i]
+                print(tmp3[i],min[i],max[i])
+            else:
+                tmp3[i] = int(line[i])
+
+        writer.writerow(tmp3)
+    
+    sourcefp.close()
+    resultfp.close()
+
+def normalization():
+    source = 'standard_data_large.csv'
+    result = 'normal_data_large.csv'
+    sourcefp = open(source , 'r')
+    resultfp = open(result , 'w' , newline='')
+    reader = csv.reader(sourcefp)
+    writer = csv.writer(resultfp)
+
+    contlist = [1,5,6,8,9,10,11]
+    tmp = np.zeros(13) 
+    for line in reader:
+        for i in range(0 , 13 , 1):
+            if i in contlist:
+                if float(max[i])-float(min[i]) == 0:
+                    tmp[i] = 0
+                else:
+                    tmp[i] = round((float(line[i])-float(min[i]))/(float(max[i])-float(min[i])) , 4)
+            else:
+                tmp[i] = line[i]
+
+        writer.writerow(tmp)
+    
+    sourcefp.close()
+    resultfp.close()
+
+
+if __name__=='__main__':
+    handleData()
+    print("first handle is over")
+    standardization()
+    print(avg)
+    print(stad.round(8))
+    print(min)
+    print(max)
+    normalization()
+    
+
